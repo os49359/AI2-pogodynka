3eb5e94326c7aead32090dda1750b63bf33422c6
os49359 <os49359@zut.edu.pl>
2023-10-26 18:36:56 +0200

    finish lab D

---
+++ src/Form/ForecastType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Forecast;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ForecastType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('cloudiness', NumberType::class, [
+                'html5' => true,
+            ])
+            ->add('temperature', NumberType::class, [
+                'html5' => true,
+            ])
+            ->add('wind', NumberType::class, [
+                'html5' => true,
+            ])
+            ->add('precipation', NumberType::class, [
+                'html5' => true,
+            ])
+            ->add('pressure', NumberType::class, [
+                'html5' => true,
+            ])
+            ->add('humidity', NumberType::class, [
+                'html5' => true,
+            ])
+            ->add('date', DateType::class, [
+                'widget' => 'single_text',
+                'html5' => true,
+            ])
+            ->add('location', EntityType::class, [
+                'class' => 'App\Entity\Location',
+                'choice_label' => 'city',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Forecast::class,
+        ]);
+    }
+}
